22a4a70c73220cf04a3f55c03e0dd25944d558ff
kaushalpatel922 <kaushalpatel922@gmail.com>
2016-09-06 20:11:45 -0400

    adding title and price

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,4 +1,10 @@
 app.controller('MainController',
-['$scope', function($scope) {
-  $scope.title = 'Top Sellers in Books';
-}]);
+  ['$scope', function($scope) {
+    $scope.title = 'Shopping Cart';
+    $scope.promo = 'DEALS';
+    $scope.product = {
+      name: 'The Book of Trees',
+      price: 19
+    };
+  }]
+);
